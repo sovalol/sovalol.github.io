fe7686840e621ed8836b7bcfcd3274ad8d301345
sovalol <36800227+sovalol@users.noreply.github.com>
2018-03-30 19:38:40 +0300

    Урок 1, сложное задание

---
+++ JavaScript/Lesson_1_hard/js/scripts.js
@@ -0,0 +1,47 @@
+/*var budj = prompt('Ваш бюджет?', '');
+var shopname = prompt('Название вашего магазина?', '');
+var shopGoods = ([prompt('Какой тип товаров будем продавать?', ''),
+				  prompt('Какой тип товаров будем продавать?', ''),
+				  prompt('Какой тип товаров будем продавать?', '')]);
+employers = {
+	Сотрудник: "Сотрудник-1",
+	Имя: "Кирилл",
+	Возраст: 22,
+	Пол: "мужской",
+	open: true,
+	Сотрудник: "Сотрудник-2",
+	Имя: "Александр",
+	Возраст: 31,
+	Пол: "мужской",
+	open: true,
+	Сотрудник: "Сотрудник-3",
+	Имя: "Ольга",
+	Возраст: 28,
+	Пол: "женский",
+	open: false
+};
+
+console.log(budj, 'рублей');
+console.log(shopname);
+
+var mainList = {
+	budj,
+	shopname,
+	shopGoods,
+	employers,
+	open: true
+};
+
+console.log(mainList);
+
+console.log(shopGoods);
+
+alert(budj/30);*/
+
+var num = "33721";
+Numb = (num[0] * num[1] * num[2] * num[3] * num[4]);
+
+console.log(Numb);
+console.log(Numb**3);
+
+alert(Numb**3);
